bd1783e2c3ebac46da0ed8791ebe2a35c0fd800d
leesoobeen <02ggang9@gmail.com>
2024-02-13 17:28:43 +0900

    feat: 코틀린 인 액션 파트 3 정리

--- kotlin-baseball/src/test/kotlin/baseball/KtInActionPart2.kt
+++ kotlin-baseball/src/test/kotlin/baseball/KtInActionPart2.kt
@@ -141,24 +141,5 @@ class KtInActionPart2Test {
         }
     }
 
-    @Test
-    fun testJoinToString() {
-        val list = listOf(1, 2, 3)
-        println(joinToString(list, "; ", "(", ")"))
-    }
-
-    private fun <T> joinToString(collection: Collection<T>,
-                                 separator: String,
-                                 prefix: String,
-                                 postfix: String): String {
-        val result = StringBuilder(prefix)
-        for ((index, element) in collection.withIndex()) {
-            if (index > 0) result.append(separator)
-            result.append(element)
-        }
-
-        result.append(postfix)
-        return result.toString()
-    }
 
 }
