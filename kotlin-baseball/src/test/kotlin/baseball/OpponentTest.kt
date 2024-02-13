e13366e6c555227223b324084d96fcb912cb9bb8
WoeunJung <leena09123@naver.com>
2024-02-14 00:39:35 +0900

    test: Opponent's success, fail test

---
+++ kotlin-baseball/src/test/kotlin/baseball/OpponentTest.kt
@@ -0,0 +1,21 @@
+package baseball
+
+import camp.nextstep.edu.missionutils.test.NsTest
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+
+class OpponentTest : NsTest() {
+
+    private val opponent = Opponent()
+
+    @Test
+    fun `상대방 숫자 선택`() {
+        opponent.getAnswerNumber()
+        val result = opponent.answerNumber
+        val isValid = result.size == 3 && result.all { it in 1..9 }
+        assertTrue(isValid)
+    }
+
+    override fun runMain() {
+    }
+}
