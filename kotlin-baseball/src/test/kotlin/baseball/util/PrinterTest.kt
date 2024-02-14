982514a31c443108b1e640133131996270febb49
WoeunJung <leena09123@naver.com>
2024-02-14 21:30:00 +0900

    test: get PlayerState by value and printer test

---
+++ kotlin-baseball/src/test/kotlin/baseball/util/PrinterTest.kt
@@ -0,0 +1,43 @@
+package baseball.util
+
+import baseball.data.Result
+import camp.nextstep.edu.missionutils.test.NsTest
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class PrinterTest : NsTest() {
+
+    private val printer = Printer()
+
+    @Test
+    fun `낫싱`() {
+        val result = Result(0,0)
+        printer.printResult(result)
+        assertEquals("낫싱", output())
+    }
+
+    @Test
+    fun `스트라이크만`() {
+        val result = Result(0,2)
+        printer.printResult(result)
+        assertEquals("2스트라이크", output())
+    }
+
+    @Test
+    fun `볼만`() {
+        val result = Result(2,0)
+        printer.printResult(result)
+        assertEquals("2볼", output())
+    }
+
+    @Test
+    fun `스트라이크, 볼 둘 다`() {
+        val result = Result(1,1)
+        printer.printResult(result)
+        assertEquals("1볼 1스트라이크", output())
+    }
+
+    override fun runMain() {
+
+    }
+}
