d5f2d1dcce49a3413868c8f532851b3db1ecf19e
stopmin <geemin0@naver.com>
2024-02-15 23:16:43 +0900

    feat: 사용자의 입력을 받아 숫자로 반환
    - 예외처리 로직 추가
    - map으로 타입 변경

---
+++ kotlin-baseball/src/main/kotlin/baseball/module/BaseballInputModule.kt
@@ -0,0 +1,14 @@
+package baseball.module
+
+import baseball.common.ErrorMessage.ILLEGAL_INPUT
+
+class BaseballInputModule {
+    fun readNumbers(): List<Int> {
+        return sequenceOf(readLine())
+            .map { it ?: throw IllegalStateException(ILLEGAL_INPUT.message) }
+            .filter { it.length == 3 && it.all { it.isDigit() } }
+            .map { it.map { char -> char.toString().toInt() } }
+            .firstOrNull()
+            ?: throw IllegalArgumentException(ILLEGAL_INPUT.message)
+    }
+}
