b64ee6ebd3769625b723b94654514afa817b29a9
stopmin <geemin0@naver.com>
2024-02-15 21:29:32 +0900

    feat: 숫자 랜덤 생성기 인터페이스 생성
    추상화가 필요할 수도 있겠다고 생각함

---
+++ kotlin-baseball/src/main/kotlin/baseball/NumberGenerator.kt
@@ -0,0 +1,11 @@
+package baseball
+
+interface NumberGenerator {
+    fun generateRandomNumber(): Int
+    fun generateThreeRandomNumbers(): List<Int>
+
+    companion object {
+        const val MIN_VALUE: Int = 1
+        const val MAX_VALUE: Int = 9
+    }
+}
