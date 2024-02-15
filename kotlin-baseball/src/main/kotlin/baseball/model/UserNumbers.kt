d9359e1ebe410510e3f85f25226a3589b2b7ea85
shkisme <shkisme0130@gmail.com>
2024-02-15 17:28:08 +0900

    feat: 유저의 숫자 생성 기능 구현

---
+++ kotlin-baseball/src/main/kotlin/baseball/model/UserNumbers.kt
@@ -0,0 +1,43 @@
+package baseball.model
+
+class UserNumbers(private val numbers: List<Int>) {
+
+    init {
+        validateSize(numbers)
+        validateRange(numbers)
+        validateDuplicate(numbers)
+    }
+
+    private fun validateSize(numbers: List<Int>) {
+        if (numbers.size != NUMBERS_SIZE) {
+            throw IllegalArgumentException("${NUMBERS_SIZE}개의 숫자를 입력해주세요.")
+        }
+    }
+
+    private fun validateRange(numbers: List<Int>) {
+        if (numbers.any { it < MIN_NUMBER || it > MAX_NUMBER }) {
+            throw IllegalArgumentException("숫자의 범위는 ${MIN_NUMBER}이상 ${MAX_NUMBER}이하입니다.")
+        }
+    }
+
+    private fun validateDuplicate(numbers: List<Int>) {
+        if (numbers.toSet().size != numbers.size) {
+            throw IllegalArgumentException("숫자는 중복될 수 없습니다.")
+        }
+    }
+
+    infix fun compareTo(randomNumbers: RandomNumbers): GameResult {
+        var strike = 0
+        var ball = 0
+
+        for (index in numbers.indices) {
+            val (userNumber, randomNumber) = numbers[index] to randomNumbers.numbers[index]
+            if (userNumber == randomNumber) {
+                strike++
+            } else if (randomNumbers.contains(userNumber)) {
+                ball++
+            }
+        }
+        return GameResult(strike = strike, ball = ball)
+    }
+}
