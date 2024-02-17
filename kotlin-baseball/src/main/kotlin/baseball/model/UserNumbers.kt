87b03c9cd51ecd2f02d5b3b199965c5df9c78d16
shkisme <shkisme0130@gmail.com>
2024-02-17 18:25:03 +0900

    refactor: require로 값 유효성을 검증하도록 수정

--- kotlin-baseball/src/main/kotlin/baseball/model/UserNumbers.kt
+++ kotlin-baseball/src/main/kotlin/baseball/model/UserNumbers.kt
@@ -3,27 +3,9 @@ package baseball.model
 class UserNumbers(private val numbers: List<Int>) {
 
     init {
-        validateSize(numbers)
-        validateRange(numbers)
-        validateDuplicate(numbers)
-    }
-
-    private fun validateSize(numbers: List<Int>) {
-        if (numbers.size != NUMBERS_SIZE) {
-            throw IllegalArgumentException("${NUMBERS_SIZE}개의 숫자를 입력해주세요.")
-        }
-    }
-
-    private fun validateRange(numbers: List<Int>) {
-        if (numbers.any { it < MIN_NUMBER || it > MAX_NUMBER }) {
-            throw IllegalArgumentException("숫자의 범위는 ${MIN_NUMBER}이상 ${MAX_NUMBER}이하입니다.")
-        }
-    }
-
-    private fun validateDuplicate(numbers: List<Int>) {
-        if (numbers.toSet().size != numbers.size) {
-            throw IllegalArgumentException("숫자는 중복될 수 없습니다.")
-        }
+        require(numbers.size == NUMBERS_SIZE)
+        require(numbers.any { it in MIN_NUMBER..MAX_NUMBER })
+        require(numbers.toSet().size == numbers.size)
     }
 
     infix fun compareTo(otherNumbers: List<Int>): GameResult {
