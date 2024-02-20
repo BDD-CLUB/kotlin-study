a1f71515ab9a2db3344f051e0a7d32af3af4c15f
leesoobeen <02ggang9@gmail.com>
2024-02-21 02:27:40 +0900

    chore: 주석 추가

--- kotlin-lotto/src/main/kotlin/lotto/model/LottoGameManager.kt
+++ kotlin-lotto/src/main/kotlin/lotto/model/LottoGameManager.kt
@@ -13,15 +13,16 @@ class LottoGameManager(
         // STEP1 > 구입금액 입력 받기
         val purchaseAmount = lottoController.purchaseLotto()
 
+        // STEP2 > 구매 내역 출력
         val randomLottos = lottoController.announceLottoNumbers(purchaseAmount)
 
-        // STEP2 > 당첨 번호 입력 받기
+        // STEP3 > 당첨 번호 입력 받기
         val winningNumbers = lottoController.enterWinningNumbers()
         
-        // STEP3 > 보너스 번호 입력 받기
+        // STEP4 > 보너스 번호 입력 받기
         val bonusNumber = lottoController.enterBonusNumber()
 
-        // STEP4 > 로또 당첨 내역 출력
+        // STEP5 > 로또 당첨 내역 출력
         val lottoBuyer = LottoBuyer(
                 randomLottos,
                 winningNumbers,
