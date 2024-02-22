88644b62309d32e0d27f6a3b320bbe65060b40f5
stopmin <geemin0@naver.com>
2024-02-23 00:04:59 +0900

    refactor: 입력받는 로직 수정

--- kotlin-lotto/src/main/kotlin/lotto/LottoStore.kt
+++ kotlin-lotto/src/main/kotlin/lotto/LottoStore.kt
@@ -2,7 +2,10 @@ package lotto
 
 import camp.nextstep.edu.missionutils.Console
 import camp.nextstep.edu.missionutils.Randoms
-import lotto.common.require
+import java.lang.IllegalStateException
+import kotlin.jvm.Throws
+
+//import lotto.common.require
 
 class LottoStore {
     companion object {
@@ -12,6 +15,26 @@ class LottoStore {
         const val LOTTO_PRICE = 1000
     }
 
+    fun receivePaymentForLotto(): Pair<Int, Int> {
+        var purchasePrice: Int?
+        println("구입급액을 입력해주세요.")
+        do {
+            val input = readLine()
+            purchasePrice = input?.toIntOrNull()
+            if (purchasePrice == null) {
+                println("[ERROR] 숫자로 입력해야합니다.")
+                throw NumberFormatException("[ERROR] 숫자로 입력해야합니다.")
+            } else if (purchasePrice % LOTTO_PRICE != 0) {
+                println("[ERROR] 숫자는 $LOTTO_PRICE(으)로 나누어 떨어져야 합니다.")
+                purchasePrice = null // 입력이 유효하지 않으므로 null로 설정하여 다시 입력 받음
+            }
+            // 유효한 입력을 받으면 자동으로 반복문에서 탈출
+        } while (purchasePrice == null)
+
+        println()
+        return Pair(purchasePrice!!, purchasePrice / LOTTO_PRICE)
+    }
+
     fun askWinningNumbers(): List<Int> {
         println("당첨 번호를 입력해주세요. 번호는 쉼표(,)로 구분하여 입력하세요:")
         var numbers: List<Int>
@@ -19,14 +42,14 @@ class LottoStore {
             numbers = readLine()?.split(",")?.mapNotNull { it.trim().toIntOrNull() } ?: emptyList()
 
             require(numbers.size == LOTTO_NUMBERS_PER_TICKET) {
-                IllegalArgumentException("[ERROR] 정확히 ${LOTTO_NUMBERS_PER_TICKET}개의 숫자를 입력해야 합니다.")
+                IllegalArgumentException("[ERROR] 정확히 $LOTTO_NUMBERS_PER_TICKET}개의 숫자를 입력해야 합니다.")
             }
+
             numbers.forEach { number ->
                 require(number in LOTTO_NUMBER_MIN..LOTTO_NUMBER_MAX) {
                     IllegalArgumentException("[ERROR] 숫자는 ${LOTTO_NUMBER_MIN}과 ${LOTTO_NUMBER_MAX} 사이여야 합니다.")
                 }
             }
-
         } while (numbers.size != LOTTO_NUMBERS_PER_TICKET
             || !numbers.all { it in LOTTO_NUMBER_MIN..LOTTO_NUMBER_MAX }
         )
@@ -38,32 +61,22 @@ class LottoStore {
 
     fun askBonusNumber(): Int {
         println("보너스 번호를 입력해 주세요.")
-        var bonusNumber: Int
-        do {
-            bonusNumber = Console.readLine().toInt()
-            require(bonusNumber in LOTTO_NUMBER_MIN..LOTTO_NUMBER_MAX) { IllegalArgumentException("[ERROR] 숫자는 ${LOTTO_NUMBER_MIN}과 ${LOTTO_NUMBER_MAX} 사이여야 합니다.") }
-        } while (bonusNumber !in LOTTO_NUMBER_MIN..LOTTO_NUMBER_MAX)
-        println()
-
-        return bonusNumber
-    }
-
-    fun receivePaymentForLotto(): Pair<Int, Int> {
-        var purchasePrice: Int?
-        println("구입급액을 입력해주세요.")
+        var bonusNumber: Int?
         do {
             try {
-                val input = Console.readLine()
-                purchasePrice = input?.toInt()
-            } catch (e: NumberFormatException) {
-                println("[ERROR] 올바른 숫자를 입력해주세요.")
-                purchasePrice = null
+                bonusNumber = Console.readLine().toInt()
+                check(bonusNumber in LOTTO_NUMBER_MIN..LOTTO_NUMBER_MAX) { NumberFormatException() }
+            } catch (e: IllegalArgumentException) {
+                bonusNumber = null
+                println("[ERROR] 정확한 숫자 형식으로 입력해주세요.")
+            } catch (e: IllegalStateException) {
+                bonusNumber = null
+                println("[ERROR] 숫자는 ${LOTTO_NUMBER_MIN}과 ${LOTTO_NUMBER_MAX} 사이여야 합니다.")
             }
-
-        } while (purchasePrice == null)
-
+        } while (bonusNumber !is Int)
         println()
-        return Pair(purchasePrice, purchasePrice / LOTTO_PRICE)
+
+        return bonusNumber!!
     }
 
 
--- kotlin-lotto/src/main/kotlin/lotto/common/ValidationUtils.kt
+++
@@ -1,8 +0,0 @@
-package lotto.common
-
-fun require(condition: Boolean, createException: () -> Exception) {
-    if (!condition) {
-        val exception = createException()
-        println(exception.message)
-    }
-}
