dd90f0a72add1b9b7fd4ad306772e219c0a87be4
WoeunJung <leena09123@naver.com>
2024-03-12 11:00:33 +0900

    feat: manage  work order and print result

--- kotlin-oncall/src/main/kotlin/oncall/OncallController.kt
+++ kotlin-oncall/src/main/kotlin/oncall/OncallController.kt
@@ -5,12 +5,14 @@ import oncall.util.OutputManager
 
 class OncallController(
     private val inputManager: InputManager = InputManager(),
-    private val outputManager: OutputManager = OutputManager()
+    private val outputManager: OutputManager = OutputManager(),
 ){
 
     fun execute() {
         val oncallInformationDTO = inputManager.getInput()
 
-
+        val workOrderService = WorkOrderService(oncallInformationDTO)
+        val workerList = workOrderService.getWorkerList()
+        outputManager.printWorkerOrder(oncallInformationDTO, workerList)
     }
 }
--- kotlin-oncall/src/main/kotlin/oncall/util/OutputManager.kt
+++ kotlin-oncall/src/main/kotlin/oncall/util/OutputManager.kt
@@ -1,5 +1,9 @@
 package oncall.util
 
+import oncall.data.OncallInformationDTO
+import oncall.data.Week.Companion.getWeek
+import oncall.data.getEndDateOfMonth
+
 class OutputManager {
 
     fun printExceptionMessage() = println("$ERROR_PREFIX 유효하지 않은 입력 값입니다. 다시 입력해 주세요.")
@@ -9,6 +13,15 @@ class OutputManager {
     fun printGetWeekdayWorkerMessage() = print("평일 비상 근무 순번대로 사원 닉네임을 입력하세요> ")
 
     fun printGetHolidayWorkerMessage() = print("휴일 비상 근무 순번대로 사원 닉네임을 입력하세요> ")
+
+    fun printWorkerOrder(oncallInformationDTO: OncallInformationDTO, workerList: List<String>) {
+        val month = oncallInformationDTO.month
+        val endDate = getEndDateOfMonth(month)
+        for (date in 1..endDate) {
+            val week = oncallInformationDTO.startWeek.getWeek(month, date)
+            print("${month}월 ${date}일 $week ${workerList[date-1]}\n")
+        }
+    }
 
     companion object {
         private const val ERROR_PREFIX = "[ERROR]"
